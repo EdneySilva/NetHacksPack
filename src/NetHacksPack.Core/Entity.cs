6df31cbd64b1ad621e6f3d975cc18e100048da90
Edney Batista da Silva <edney_contato@hotmail.com>
2020-07-26 19:47:58 -0300

    Added core entity for handle business objects

---
+++ src/NetHacksPack.Core/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetHacksPack.Core
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        protected abstract Result<Entity> Validate();
+    }
+}
