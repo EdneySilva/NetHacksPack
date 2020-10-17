e83a78292cff1de5c0dff71b9dc9f05454bb3c3d
Edney Batista da Silva <edney_contato@hotmail.com>
2020-10-17 03:49:39 -0300

    Added common events to handle data operations

---
+++ src/NetHacksPack.Database.Events/DataDeletedEvent.cs
@@ -0,0 +1,12 @@
+﻿using NetHacksPack.Database.Events.Constants;
+
+namespace NetHacksPack.Database.Events
+{
+    public class DataDeletedEvent<TData> : DataEventBase<TData>
+    {
+        public DataDeletedEvent(TData data, string type) 
+            : base(data, EventTypes.DELETE)
+        {
+        }
+    }
+}
---
+++ src/NetHacksPack.Database.Events/DataUpdatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using NetHacksPack.Database.Events.Constants;
+using System;
+
+namespace NetHacksPack.Database.Events
+{
+    public class DataUpdatedEvent<TData> : DataEventBase<TData>
+    {
+        public DataUpdatedEvent(TData data, string type)
+            : base(data, EventTypes.UPDATE)
+        {
+        }
+    }
+}
