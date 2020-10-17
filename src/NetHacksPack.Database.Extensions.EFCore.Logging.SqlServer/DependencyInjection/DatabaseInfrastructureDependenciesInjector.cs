19e7d3fd007cdcb5651b09072ce11c5c76d6bd48
Edney Batista da Silva <edney_contato@hotmail.com>
2020-10-17 06:31:55 -0300

    Added log support by an entity configured to sqlserver database

---
+++ src/NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetHacksPack.Database.Extension.EFCore.Logging.DependencyInjection;
+using NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer.Configuration;
+
+namespace NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer.DependencyInjection
+{
+    public static class DatabaseInfrastructureDependenciesInjector
+    {
+        public static LoggerExtensionBuilder AddLoggingExtensionForEFSqlServer(this IServiceCollection services, UserProvider userProvider)
+        {
+            return services.AddLoggingExtensionForEF(userProvider, (services) =>
+            {
+                return new SqlEventLogConfiguration();
+            });
+        }
+    }
+}
