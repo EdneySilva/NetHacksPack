3cc1976261d41c7997a970bc275b86ae1068dd80
Edney Batista da Silva <edney_contato@hotmail.com>
2020-10-17 06:29:49 -0300

    Added event to be applied when the dbcontext apply the configurations

---
+++ src/NetHacksPack.Database.Events/ApplyConfigurationsToContextCommand.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetHacksPack.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetHacksPack.Database.Events
+{
+    public class ApplyConfigurationsToContextCommand : Command
+    {
+        private readonly ModelBuilder modelBuilder;
+
+        public ApplyConfigurationsToContextCommand(ModelBuilder modelBuilder)
+            : base()
+        {
+            this.modelBuilder = modelBuilder;
+        }
+
+        protected ApplyConfigurationsToContextCommand()
+        {
+        }
+
+        public void UseConfiguration<TEntity>(IEntityTypeConfiguration<TEntity> instance) where TEntity : class
+        {
+            this.modelBuilder.ApplyConfiguration(instance);
+        }
+    }
+}
