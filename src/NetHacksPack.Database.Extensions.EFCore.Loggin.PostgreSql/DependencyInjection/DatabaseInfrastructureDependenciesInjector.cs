ab630946851b233fbe611e002f2769eda9153d9b
Edney Batista da Silva <edney_contato@hotmail.com>
2020-10-17 06:31:43 -0300

    Added log support by an entity configured to postgresql database

---
+++ src/NetHacksPack.Database.Extensions.EFCore.Loggin.PostgreSql/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetHacksPack.Database.Extension.EFCore.Logging.DependencyInjection;
+using NetHacksPack.Database.Extensions.EFCore.Logging.PostgreSql.Configuration;
+
+namespace NetHacksPack.Database.Extensions.EFCore.Logging.PostgreSql.DependencyInjection
+{
+    public static class DatabaseInfrastructureDependenciesInjector
+    {
+        public static LoggerExtensionBuilder AddLoggingExtensionForEFPostgreSql(this IServiceCollection services, UserProvider userProvider)
+        {
+            return services.AddLoggingExtensionForEF(userProvider, (services) =>
+            {
+                return new PostgreSqlEventLogConfiguration();
+            });
+        }
+    }
+}
