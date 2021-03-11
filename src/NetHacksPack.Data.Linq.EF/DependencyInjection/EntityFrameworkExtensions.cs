9e6f8b35a17c0db238826856433e23482fc3f091
Edney Batista da Silva <edney_contato@hotmail.com>
2021-03-11 11:06:44 -0300

    added project to create queryable as repostiories

---
+++ src/NetHacksPack.Data.Linq.EF/DependencyInjection/EntityFrameworkExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NetHacksPack.Data.Linq.EF;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IServiceCollection AddTransientUnityOfWork<TDbContext>(this IServiceCollection services)
+            where TDbContext : DbContext
+        {
+            return services
+                .AddScoped(typeof(IQueryable<>), typeof(EfQuery<>));
+        }
+    }
+}
---
+++ src/NetHacksPack.Data.Linq.EF/EfQuery.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections;
+
+namespace NetHacksPack.Data.Linq.EF
+{
+    internal class EfQuery<TEntity> : IQueryable<TEntity>
+        where TEntity : class
+    {
+        private readonly DbContext dbContext;
+        private readonly IQueryable<TEntity> dbSet;
+
+        public EfQuery(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbSet = this.dbContext.Set<TEntity>().AsQueryable();
+        }
+        public Type ElementType => dbSet.ElementType;
+
+        public Expression Expression => dbSet.Expression;
+
+        public IQueryProvider Provider => dbSet.Provider;
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return dbSet.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return dbSet.GetEnumerator();
+        }
+    }
+}
