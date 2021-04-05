fe5734d96b8de653ae68d8c86a199f9a29d4f400
Edney Batista da Silva <edney_contato@hotmail.com>
2021-04-05 08:03:46 -0300

    added statements to get schemama, table name, and register the command provider

--- src/NetHacksPack.Database.Extensions.EFCore.Loggin.PostgreSql/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
+++ src/NetHacksPack.Database.Extensions.EFCore.Loggin.PostgreSql/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using NetHacksPack.Database.Extension.EF.Infrastructure;
 using NetHacksPack.Database.Extension.EFCore.Logging.DependencyInjection;
+using NetHacksPack.Database.Extension.EFCore.Logging.Providers;
 using NetHacksPack.Database.Extensions.EFCore.Logging.PostgreSql.Configuration;
+using NetHacksPack.Database.Extensions.EFCore.Logging.PostgreSql.Infrastructure;
 
 namespace NetHacksPack.Database.Extensions.EFCore.Logging.PostgreSql.DependencyInjection
 {
     public static class DatabaseInfrastructureDependenciesInjector
     {
-        public static LoggerExtensionBuilder AddLoggingExtensionForEFPostgreSql<TDbContext>(this IServiceCollection services, UserProvider userProvider)
+        public static LoggerExtensionBuilder AddLoggingExtensionForEFPostgreSql<TDbContext>(this IServiceCollection services, string tableName, UserProvider userProvider, string migrationTableName, string schemma = null)
             where TDbContext : DbContext
         {
-            return services.AddLoggingExtensionForEF<TDbContext>(userProvider, (services) =>
+            services.AddScoped<IHistoricalRepository, PostgreSqlHistoricalRepository>();
+            services.AddScoped<IDatabaseCommandGenerator, PostgreSqlServerCommandGenerator>();
+            services.AddSingleton<SchemmaNameProvider>(() => (schemma?.Length > 0 ? schemma : string.Empty));
+            services.AddSingleton<MigrationTableNameProvider>(() =>
             {
-                return new PostgreSqlEventLogConfiguration();
+                return (schemma?.Length > 0 ? "\"" + schemma + "\"." : string.Empty) + "\"" + migrationTableName + "\"";
+            });
+            return services.AddLoggingExtensionForEF<TDbContext>(tableName, userProvider, (services) =>
+            {
+                return new PostgreSqlEventLogConfiguration(tableName);
             });
         }
     }
