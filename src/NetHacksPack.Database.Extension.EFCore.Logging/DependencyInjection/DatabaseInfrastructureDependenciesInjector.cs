bccac743600b10832fd3d3d5027ee0b8aa23482d
Edney Batista da Silva <edney_contato@hotmail.com>
2021-04-05 08:03:14 -0300

    added new necessary dependencies to recognize table name, and audit database provider

--- src/NetHacksPack.Database.Extension.EFCore.Logging/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
+++ src/NetHacksPack.Database.Extension.EFCore.Logging/DependencyInjection/DatabaseInfrastructureDependenciesInjector.cs
@@ -4,6 +4,7 @@ using NetHacksPack.Database.Extension.EFCore.Logging.Configuration;
 using System.Collections.Generic;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using NetHacksPack.Database.Extension.EFCore.Logging.Providers;
 
 namespace NetHacksPack.Database.Extension.EFCore.Logging.DependencyInjection
 {
@@ -22,14 +23,16 @@ namespace NetHacksPack.Database.Extension.EFCore.Logging.DependencyInjection
             return services;
         }
 
-        public static LoggerExtensionBuilder AddLoggingExtensionForEF<TDbContext>(this IServiceCollection services, UserProvider userProvider, EventLogsConfigurationProvider eventLogsConfigurationProvider)
+        public static LoggerExtensionBuilder AddLoggingExtensionForEF<TDbContext>(this IServiceCollection services, string auditTableName, UserProvider userProvider, EventLogsConfigurationProvider eventLogsConfigurationProvider)
             where TDbContext : DbContext
         {
             var blackList = new List<IgnoredEntity>();
             services.AddScoped<IRequestHandler<ApplyConfigurationsToContextCommand, bool>, ContextConfigurationHandler>();
+            services.AddSingleton<AuditTableNameProvider>(() => auditTableName);
             services.AddSingleton(userProvider);
+            services.AddSingleton(blackList);
             services.AddSingleton(eventLogsConfigurationProvider);
-            services.AddSingleton(blackList); 
+            services.AddScoped<IDatabaseAuditProvider, DatabaseAuditProvider>();
             services.DisableAllLogOn<EventLog>();
             services.AddScoped<DbContextProvider>((serviceProvider) =>
             {
