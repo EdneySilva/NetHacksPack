60ded3a7d734caba63bd7261ed0b2d3db3de848c
Edney Batista da Silva <edney_contato@hotmail.com>
2021-04-05 08:01:16 -0300

    created repositories to handle migrations internally, but we are studing to remove soon

---
+++ src/NetHacksPack.Database.Extension.EFCore.Logging/Infrastructure/HistoricalRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetHacksPack.Database.Extension.EF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetHacksPack.Database.Extension.EFCore.Logging.Infrastructure
+{
+    class HistoricalRepository : IHistoricalRepository
+    {
+        private readonly DbContext dbContext;
+        private readonly IDatabaseCommandGenerator databaseCommandGenerator;
+
+        public HistoricalRepository(DbContext dbContext, IDatabaseCommandGenerator databaseCommandGenerator)
+        {
+            this.dbContext = dbContext;
+            this.databaseCommandGenerator = databaseCommandGenerator;
+        }
+
+        public string GetInsertScript(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasEventLogTable(string migrationId)
+        {
+            return this.dbContext.Database.GetAppliedMigrations().Any(a => a == migrationId);
+        }
+    }
+}
---
+++ src/NetHacksPack.Database.Extension.EFCore.Logging/Infrastructure/IHistoricalRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace NetHacksPack.Database.Extension.EFCore.Logging.Infrastructure
+{
+    interface IHistoricalRepository2
+    {
+        bool HasMisgration(string migrationId);
+
+        string GetInsertScript(string key, string value);
+    }
+}
