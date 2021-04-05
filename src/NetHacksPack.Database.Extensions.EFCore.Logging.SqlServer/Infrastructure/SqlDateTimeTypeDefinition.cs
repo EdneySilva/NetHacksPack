47b55ddf62ed999b93a8d91b67f35cf21db6df30
Edney Batista da Silva <edney_contato@hotmail.com>
2021-04-05 07:59:36 -0300

    created sql commands

---
+++ src/NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer/Infrastructure/SqlDateTimeTypeDefinition.cs
@@ -0,0 +1,14 @@
+﻿using NetHacksPack.Database.Extension.EF.Infrastructure;
+
+namespace NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer.Infrastructure
+{
+    class SqlDateTimeTypeDefinition : ColumnTypeCommandDefinition
+    {
+        public override string CreateCommand()
+        {
+            return "DATETIME";
+        }
+    }
+
+
+}
---
+++ src/NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer/Infrastructure/SqlVarcharTypeDefinition.cs
@@ -0,0 +1,20 @@
+﻿using NetHacksPack.Database.Extension.EF.Infrastructure;
+
+namespace NetHacksPack.Database.Extensions.EFCore.Logging.SqlServer.Infrastructure
+{
+    class SqlVarcharTypeDefinition : ColumnTypeCommandDefinition
+    {
+        private readonly int lenght;
+
+        public SqlVarcharTypeDefinition(int lenght)
+        {
+            this.lenght = lenght;
+        }
+        public override string CreateCommand()
+        {
+            return $"VARCHAR({lenght})";
+        }
+    }
+
+
+}
